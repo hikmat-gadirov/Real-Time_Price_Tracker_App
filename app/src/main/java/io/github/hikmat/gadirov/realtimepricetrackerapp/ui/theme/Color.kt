2b40c63e2ed7fc287e58ca4e174f8accdcb5c1fb
Hikmat Gadirov <hikmet.kadirov.96@gmail.com>
2026-04-04 11:03:00 +0400

    Updated app theme, colors, and string resources

--- app/src/main/java/io/github/hikmat/gadirov/realtimepricetrackerapp/ui/theme/Color.kt
+++ app/src/main/java/io/github/hikmat/gadirov/realtimepricetrackerapp/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val TransparentGreen = Color(0x3300FF00)
+
+val TransparentRed = Color(0x33FF0000)
+
+val SolidGreen = Color(0xFF4CAF50)
+
+val SolidRed = Color(0xFFF44336)
